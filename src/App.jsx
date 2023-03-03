336a4f5ddbdaad41bb389f79979e46ad1ac62c1c
EthanSibaja <L19350565@tuxtepec.tecnm.mx>
2023-03-02 21:05:20 -0600

    Modifica App para mostrar los botones de Incrementar, Restablecer y Decrementar

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <h3>Contador</h3>
       <hr />
       <h1>{Counter}</h1>
+      <hr />
       <button 
       onClick={()=> {
         setCounter(Counter + 1);
@@ -17,6 +18,24 @@ function App() {
       type="button" 
       className="btn btn-primary"
       >+1</button>
+      <hr />
+      <button 
+      onClick={()=> {
+        setCounter(0);
+        console.log(0)
+      }}
+      type="button" 
+      className="btn btn-success"
+      >Restablecer</button>
+      <hr />
+      <button 
+      onClick={()=> {
+        setCounter(Counter - 1);
+        console.log(Counter)
+      }}
+      type="button" 
+      className="btn btn-secondary"
+      >-1</button>
     </div>
   )
 }
